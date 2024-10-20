ec051a23f58f41322b5d03a9c80ba64501298e78
YusukeMizutani11 <122545158+YusukeMizutani11@user.noreply.github.com>
2024-10-20 14:20:19 -0500

    implement fetchPrefectures & fetchPopulation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
- return <div className="mt-4 text-gray-500">Hi, this is home page</div>;
-}
+const Home = () => {
+ return <div>Home Page</div>;
+};
+
+export default Home;
---
+++ src/utils/api.ts
@@ -0,0 +1,72 @@
+const API_KEY = process.env.NEXT_PUBLIC_RESAS_API_KEY;
+const BASE_URL = process.env.NEXT_PUBLIC_RESAS_BASE_URL;
+
+interface PopulationData {
+ year: number;
+ value: number;
+}
+
+interface PopulationCategory {
+ label: string;
+ data: PopulationData[];
+}
+
+interface PopulationResponse {
+ message: string | null;
+ result: {
+  boundaryYear: number;
+  data: PopulationCategory[];
+ };
+}
+
+export const fetchPrefectures = async () => {
+ try {
+  const res = await fetch(`${BASE_URL}/prefectures`, {
+   headers: { 'X-API-KEY': API_KEY! },
+  });
+
+  if (!res.ok) {
+   throw new Error(`Error fetching prefectures: ${res.status}`);
+  }
+
+  const data = await res.json();
+
+  return data.result;
+ } catch (error) {
+  console.error('Error fetching prefectures:', error);
+  return undefined;
+ }
+};
+
+export const fetchPopulation = async (
+ prefCode: number,
+ category = '総人口',
+): Promise<PopulationData[] | undefined> => {
+ try {
+  const res = await fetch(
+   `${BASE_URL}/population/composition/perYear?prefCode=${prefCode}`,
+   {
+    headers: { 'X-API-KEY': API_KEY! },
+   },
+  );
+
+  if (!res.ok) {
+   throw new Error(`Error fetching population: ${res.status}`);
+  }
+
+  const data: PopulationResponse = await res.json();
+
+  const population = data.result.data.find(
+   (d: PopulationCategory) => d.label === category,
+  );
+
+  if (!population) {
+   throw new Error('Category not found');
+  }
+
+  return population.data;
+ } catch (error) {
+  console.error('Error fetching population:', error);
+  return undefined;
+ }
+};
